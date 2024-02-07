a8baccea7d8c7fdb9ad3de3290486d4bbafef845
juanib99 <juanignaciobisello@hotmail.com>
2024-02-06 23:11:47 -0300

    adding fech

--- src/breackingbad/breacking-bad.js
+++ src/breackingbad/breacking-bad.js
@@ -1,10 +1,27 @@
 
 /**
+ * @returns {Promise<Object>} quote information
+ */
+const fetchQuote = async() => {
+        const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+
+        const data = await res.json();
+
+        console.log(data[0])
+        return data[0];
+    } 
+
+
+/**
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreackingBadApp = ( element ) => {
+export const BreackingBadApp = async( element ) => {
     document.querySelector('#app-title').innerHTML = 'Breacking Bad';
     
     element.innerHTML = 'Loading...';
+
+    const quote = await fetchQuote();
+
+    element.innerHTML = 'Hay datos';
 }
