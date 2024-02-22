22032ea650678ab7d98b51ffd35a466fc424a417
juanib99 <juanignaciobisello@hotmail.com>
2024-02-21 23:30:49 -0300

    finalizacion del CRUD

--- src/users/presentation/render-table/render-table.js
+++ src/users/presentation/render-table/render-table.js
@@ -1,4 +1,6 @@
 import usersStore from '../../store/users-store';
+import { deleteUserById } from '../../uses-cases/delete-user-by-id';
+import { showModal } from '../render-modal/render-modal';
 import './render-table.css';
 
 let table;
@@ -21,6 +23,42 @@ const createTable = () => {
     table.append( tableHeaders, tableBody )
     return table;
 }
+
+/**
+ * 
+ * @param {MouseEvent} event 
+ */
+const tableSelectListener = ( event ) => {
+    const element = event.target.closest('.select-user');
+    if ( !element ) return;
+
+    const id = element.getAttribute('data-id');
+    showModal(id);
+}
+
+
+/**
+ * 
+ * @param {MouseEvent} event 
+ */
+const tableDeleteListener = async( event ) => {
+    const element = event.target.closest('.delete-user');
+    if ( !element ) return;
+
+    const id = element.getAttribute('data-id');
+    try {
+        await deleteUserById( id );
+        await usersStore.reloadPage();
+        document.querySelector('#current-page').innerText = usersStore.getCurrentPage();
+        renderTable();
+
+    } catch ( error ) {
+        console.logq(error);
+        alert('No se pudo eliminar');
+    }
+}
+
+
 /**
  * 
  * @param {HTMLDivElement} element 
@@ -34,6 +72,8 @@ export const renderTable = ( element ) => {
         element.append( table );
 
         //TODO: listener a la tabla
+        table.addEventListener('click', tableSelectListener );
+        table.addEventListener('click', tableDeleteListener );
     }
 
 
@@ -48,9 +88,9 @@ export const renderTable = ( element ) => {
             <td>${ user.lastName }</td>
             <td>${ user.isActive }</td>
             <td>
-                <a href"#/" data-id="${ user.id }" >Select</a>
+                <a href"#/" class="select-user" data-id="${ user.id }" >Select</a>
                 
-                <a href"#/" data-id="${ user.id }" >Delete</a>
+                <a href"#/" class="delete-user" data-id="${ user.id }" >Delete</a>
             </td>
         </tr>
         `;
--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,7 +1,9 @@
 import { renderAddButton } from "./presentation/render-add-button/render-add-button";
 import { renderButtons } from "./presentation/render-buttons/render-buttons";
+import { renderModal } from "./presentation/render-modal/render-modal";
 import { renderTable } from "./presentation/render-table/render-table";
 import usersStore from "./store/users-store";
+import { saveUser } from "./uses-cases/save-user";
 
 /**
  * 
@@ -16,6 +18,11 @@ export const UsersApp = async( element ) => {
     renderTable( element );
     renderButtons( element );
     renderAddButton( element );
+    renderModal( element, async( userLike ) => {
+        const user = await saveUser( userLike);
+        usersStore.onUserChanged( user );
+        renderTable();
+    } );
 
     
 }
